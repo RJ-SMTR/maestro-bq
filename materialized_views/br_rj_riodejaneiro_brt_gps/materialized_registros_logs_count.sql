fbdef59fa87aad2c1b831acceb53930d13385482
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-08-17 10:05:21 -0300

    fix count query

--- materialized_views/br_rj_riodejaneiro_brt_gps/materialized_registros_logs_count.sql
+++ materialized_views/br_rj_riodejaneiro_brt_gps/materialized_registros_logs_count.sql
@@ -1,6 +1,6 @@
 SELECT
   COUNT(sucesso) / {{ n_capturas_hora }} as porcentagem_sucesso,
-  data,
+  SAFE_CAST(DATETIME(TIMESTAMP(data), "America/Sao_Paulo") AS DATETIME) data,
   extract(hour from timestamp_captura) as hora,
   {{ run_timestamp }} AS run_timestamp,
   {{ maestro_sha }} AS maestro_sha,
@@ -8,5 +8,3 @@ SELECT
 FROM `rj-smtr.br_rj_riodejaneiro_brt_gps.materialized_registros_logs`
 WHERE timestamp_captura >= {{ date_range_start }} AND timestamp_captura <= {{ date_range_end }}
 GROUP BY data, hora
-ORDER BY data DESC, hora DESC
-LIMIT 100
