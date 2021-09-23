8637a31937569bf772b1a168f8a22e4b109f0b08
hellcassius <caiorogerio.santos@gmail.com>
2021-09-23 16:47:48 -0300

    beautify viagens realizadas

--- smtr/br_rj_riodejaneiro_onibus_gps/viagens_realizadas.sql
+++ smtr/br_rj_riodejaneiro_onibus_gps/viagens_realizadas.sql
@@ -1,54 +1,80 @@
 -- get start/end of trips, detects vehicles coming in the buffer at each boundary point
 with 
     t as (
-        select id_veiculo,linha_gps,linha_gtfs,
-            round(distancia,1) distancia, trip_id, faixa_horaria, status,
-            data, hora, 
+        SELECT 
+            id_veiculo,
+            linha_gps,
+            linha_gtfs,
+            round(distancia,1) distancia,
+            trip_id, 
+            faixa_horaria, 
+            status,
+            data, 
+            hora, 
             string_agg(status,"") over (
-                partition by id_veiculo, trip_id
-                order by id_veiculo, trip_id, faixa_horaria
-                rows between current row and 1 following) = 'startmiddle' starts,
+                PARTITION BY id_veiculo, trip_id
+                ORDER BY id_veiculo, trip_id, faixa_horaria
+                ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING) = 'startmiddle' starts,
             string_agg(status,"") over (
-                partition by id_veiculo, trip_id
-                order by id_veiculo, trip_id, faixa_horaria
-                rows between 1 preceding and current row) = 'middleend' ends
-        from {{ intersec }}
-        where data between DATE({{ date_range_start }}) and DATE({{ date_range_end }}) 
-        and linha_gps=linha_gtfs
-        order by trip_id, faixa_horaria
+                PARTITION BY id_veiculo, trip_id
+                ORDER BY id_veiculo, trip_id, faixa_horaria
+                ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) = 'middleend' ends
+        FROM {{ intersec }}
+        WHERE 
+            data BETWEEN DATE({{ date_range_start }}) AND DATE({{ date_range_end }}) 
+            AND linha_gps=linha_gtfs
+        ORDER BY trip_id, faixa_horaria
     ),
-    s as (
-        select *,
-            case when
-            string_agg(status,"") over (
-                partition by id_veiculo, trip_id
-                order by id_veiculo, trip_id, faixa_horaria
-                rows between current row and 1 following) = 'startend' 
-            then datetime(faixa_horaria, "America/Sao_Paulo") end datetime_partida,
-            case when string_agg(status,"") over (
-                partition by id_veiculo, trip_id
-                order by id_veiculo, trip_id, faixa_horaria
-                rows between 1 preceding and current row) = 'startend' 
-            then datetime(faixa_horaria,"America/Sao_Paulo") end datetime_chegada
-        from t
-        where starts = true or ends = true
+    s AS (
+        SELECT 
+            *,
+            CASE 
+                WHEN
+                string_agg(status,"") over (
+                    PARTITION BY id_veiculo, trip_id
+                    ORDER BY id_veiculo, trip_id, faixa_horaria
+                    ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING) = 'startend' 
+                THEN faixa_horaria 
+            END datetime_partida,
+            CASE 
+                WHEN string_agg(status,"") over (
+                    PARTITION BY id_veiculo, trip_id
+                    ORDER BY id_veiculo, trip_id, faixa_horaria
+                    ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) = 'startend' 
+                THEN faixa_horaria
+            END datetime_chegada
+        FROM 
+            t
+        WHERE 
+            starts = true OR ends = true
     ),
-    w as (
-        select * except(datetime_partida), 
+    w AS (
+        SELECT 
+            * EXCEPT(datetime_partida), 
             lag(datetime_partida) over(
-            partition by id_veiculo, trip_id 
-            order by id_veiculo, trip_id, faixa_horaria) datetime_partida,
-        from s
+            PARTITION BY id_veiculo, trip_id 
+            ORDER BY id_veiculo, trip_id, faixa_horaria) datetime_partida,
+        FROM s
     ),
     realized_trips as (
-        select *,
-            1 as tipo_trajeto,
-            datetime_diff(datetime_chegada, datetime_partida, minute) as tempo_gasto, 
-            round(SAFE_DIVIDE(distancia/1000, datetime_diff(datetime_chegada, datetime_partida, minute)/60), 1) as velocidade_trajeto,
+        SELECT 
+            id_veiculo,
+            linha_gps,
+            linha_gtfs,
+            trip_id,
+            datetime_partida,
+            datetime_chegada,
+            1 AS tipo_trajeto,
+            datetime_diff(datetime_chegada, datetime_partida, minute) AS tempo_gasto, 
+            round(SAFE_DIVIDE(distancia/1000, datetime_diff(datetime_chegada, datetime_partida, minute)/60), 1) AS velocidade_trajeto,
             STRUCT({{ maestro_sha }} AS versao_maestro, {{ maestro_bq_sha }} AS versao_maestro_bq) versao
-        from w
-        where datetime_partida is not null
-        order by id_veiculo, linha_gtfs, trip_id, datetime_partida
+        FROM w
+        WHERE datetime_partida IS NOT NULL
     )
-select * from realized_trips 
-where velocidade_trajeto between {{ filtro_min_velocidade }} and {{ filtro_max_velocidade }}
+
+SELECT 
+    * 
+FROM 
+    realized_trips 
+WHERE 
+    velocidade_trajeto BETWEEN {{ filtro_min_velocidade }} AND {{ filtro_max_velocidade }}
