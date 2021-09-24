0b396a5d6b8412f38354360990f684ba1693fada
hellcassius <caiorogerio.santos@gmail.com>
2021-09-24 13:56:49 -0300

    adequate columns to new gps_sppo dependencies

--- smtr/projeto_multa_automatica/detalhes_veiculo_onibus_completa.sql
+++ smtr/projeto_multa_automatica/detalhes_veiculo_onibus_completa.sql
@@ -1,11 +1,14 @@
 select distinct
     data,
-    linha,
+    servico as linha,
     id_veiculo,
     extract(time from timestamp_seconds(60 * div(unix_seconds(timestamp(timestamp_gps)), 60))) timestamp_minuto,
-    cast(extract(time from timestamp_seconds({{ faixa_horaria * 60 }} * div(unix_seconds(timestamp(timestamp_gps)), {{ faixa_horaria * 60 }}))) as string) faixa_horaria,
+    cast(extract(time from timestamp_seconds(
+        {{ faixa_horaria * 60 }} * div(
+                                    unix_seconds(timestamp(timestamp_gps)), 
+                                    {{ faixa_horaria * 60 }}))) as string) faixa_horaria,
     case
-        when status_tipo_parada = 'garagem' then 'garagem'
+        when tipo_parada = 'garagem' then 'garagem'
         else 'operando'
     end situacao
 from {{ registros }}
