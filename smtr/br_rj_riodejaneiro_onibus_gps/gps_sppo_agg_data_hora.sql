8383423d87fae198c916f5869b694031b5e890cd
hellcassius <caiorogerio.santos@gmail.com>
2021-09-30 13:36:31 -0300

    preparing for deploy

--- smtr/br_rj_riodejaneiro_onibus_gps/gps_sppo_agg_data_hora.sql
+++ smtr/br_rj_riodejaneiro_onibus_gps/gps_sppo_agg_data_hora.sql
@@ -2,4 +2,5 @@ SELECT
   data, EXTRACT(HOUR FROM timestamp_gps) hora, count(distinct id_veiculo) n_veiculos
 FROM {{ gps_sppo }}
 where not tipo_parada = 'garagem' AND servico IS NOT NULL
+and data between DATE({{ date_range_start }}) and DATE({{ date_range_end }})
 GROUP BY data, EXTRACT(HOUR FROM timestamp_gps)
--- smtr/br_rj_riodejaneiro_onibus_gps/gps_sppo_agg_data_hora_consorcio.sql
+++ smtr/br_rj_riodejaneiro_onibus_gps/gps_sppo_agg_data_hora_consorcio.sql
@@ -7,7 +7,7 @@ from {{ gps_sppo }} t1
 join {{ linhas_sppo }} t2
 on t1.servico = t2.linha_completa
 where not t1.tipo_parada = "garagem"
-and t1.data between {{ date_range_start }} and {{ date_range_end }}
+and t1.data between DATE({{ date_range_start }}) and DATE({{ date_range_end }})
 group by 
     t1.data,
     EXTRACT(HOUR FROM timestamp_gps),
