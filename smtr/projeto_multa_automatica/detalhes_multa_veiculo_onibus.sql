e86408550202518395d09f97e91edd7710bcd271
Caio R S dos Santos <caiorogerio.santos@gmail.com>
2021-10-20 10:58:32 -0300

    [projeto_multa_automatica] Refatora os filtros de materialização (#21)
    * refactor filtering
    
    * change defaults for triggering materialization

--- smtr/projeto_multa_automatica/detalhes_multa_veiculo_onibus.sql
+++ smtr/projeto_multa_automatica/detalhes_multa_veiculo_onibus.sql
@@ -20,15 +20,18 @@ select
     row_number() over (partition by t1.linha, t1.data, t1.faixa_horaria, t2.tipo_multa order by t1.linha, t1.data, t1.faixa_horaria, t2.tipo_multa, t3.ordem) as row_num
 from (
     select * from {{ detalhes_veiculo_onibus_completa }}
-    where 
-    DATE(data) > date({{ date_range_start }}) 
-    and data <= date({{ date_range_end }})
+    where
+    DATE(data) between date({{ date_range_start }}) and date({{ date_range_end }})
+    and datetime(concat(cast(data as string), " ", faixa_horaria)) > {{ date_range_start }}
+    and datetime(concat(cast(data as string), " ", faixa_horaria)) <= {{ date_range_end }}
 ) t1
 join (
     select *
     from {{ detalhes_multa_linha_onibus }}
-    where DATE(data) > date({{ date_range_start }}) 
-    and data <= date({{ date_range_end }})
+    where
+    DATE(data) between date({{ date_range_start }}) and date({{ date_range_end }})
+    and datetime(concat(cast(data as string), " ", faixa_horaria)) > {{ date_range_start }}
+    and datetime(concat(cast(data as string), " ", faixa_horaria)) <= {{ date_range_end }}
 ) t2
 on t1.linha = t2.linha 
 and t1.data = t1.data
